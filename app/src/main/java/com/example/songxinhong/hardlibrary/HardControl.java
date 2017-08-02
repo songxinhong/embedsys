01a5b419a3e186e857a4e20633f1bd690a16894c
songxinhong <477941407@qq.com>
2017-08-02 18:53:12 +0800

    v2.2 complie hardcontrol.so -nostdlib, app/libs/armeabi/, revise build.gradle(module:app)

--- app/src/main/java/com/example/songxinhong/hardlibrary/HardControl.java
+++ app/src/main/java/com/example/songxinhong/hardlibrary/HardControl.java
@@ -4,13 +4,13 @@ package com.example.songxinhong.hardlibrary;
  */
 
 public class HardControl {
-    public static native int ledCtrl(int which, int status);
-    public static native int ledOpen();
+    public static native int ledCtrl(int which, int status);//native表示这个方法不是在Java中实现的而是在C语言中实现的
+    public static native int ledOpen();//static声明成静态方法可以便于调用，不必new构造出对象再调用方法，可以直接调用
     public static native void ledClose();
 
-    static {
+    static {                             //静态代码块在构造对象之前执行，并且只执行一次
         try {
-            System.loadLibrary("hardcontrol");
+            System.loadLibrary("hardcontrol");//libhardcontrol.so
         } catch (Exception e) {
             e.printStackTrace();
         }
