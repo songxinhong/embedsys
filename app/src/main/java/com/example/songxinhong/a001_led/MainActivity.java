095e95b0e8c96f5feff0892cff09192f5409f6ae
songxinhong <477941407@qq.com>
2017-08-02 19:43:28 +0800

    add __android_log_print match ledopen ledclose ledctrl in .so(c language)

--- app/src/main/java/com/example/songxinhong/a001_led/MainActivity.java
+++ app/src/main/java/com/example/songxinhong/a001_led/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HardControl.ledOpen();
         final Button button = (Button) findViewById(R.id.button);
         final CheckBox led1 = (CheckBox) findViewById(R.id.led1);
         final CheckBox led2 = (CheckBox) findViewById(R.id.led2);
@@ -22,21 +23,25 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click.
-                HardControl hardControl = new HardControl();
+
                 led_flag = !led_flag;
                 if (led_flag == false) {
-                    button.setText("ALL LED OFF");
+                    button.setText("ALL LED ON");
                     led1.setChecked(false);
                     led2.setChecked(false);
                     led3.setChecked(false);
                     led4.setChecked(false);
+                    for (int i = 0; i < 4; i++)
+                        HardControl.ledCtrl(i,0);
                 }
                 else {
-                    button.setText("ALL LED ON");
+                    button.setText("ALL LED OFF");
                     led1.setChecked(true);
                     led2.setChecked(true);
                     led3.setChecked(true);
                     led4.setChecked(true);
+                    for (int i = 0; i < 4; i++)
+                        HardControl.ledCtrl(i,1);
                 }
             }
         });
@@ -50,28 +55,36 @@ public class MainActivity extends AppCompatActivity {
         // Check which checkbox was clicked
         switch(view.getId()) {
             case R.id.led1:
-                if (checked)
+                if (checked){
                     Toast.makeText(MainActivity.this,"led1 on",Toast.LENGTH_SHORT).show();
-                 else
+                    HardControl.ledCtrl(0,1);}
+                 else {
                     Toast.makeText(MainActivity.this,"led1 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0,0);}
                 break;
             case R.id.led2:
-                if (checked)
+                if (checked){
                     Toast.makeText(MainActivity.this,"led2 on",Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this,"led2 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1,1);}
+                else {
+                    Toast.makeText(MainActivity.this, "led2 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1,0);}
                 break;
             case R.id.led3:
-                if (checked)
+                if (checked){
                     Toast.makeText(MainActivity.this,"led3 on",Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this,"led3 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2,1);}
+                else {
+                    Toast.makeText(MainActivity.this, "led3 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2,0);}
                 break;
             case R.id.led4:
-                if (checked)
+                if (checked){
                     Toast.makeText(MainActivity.this,"led4 on",Toast.LENGTH_SHORT).show();
-                else
+                    HardControl.ledCtrl(3,1);}
+                else {
                     Toast.makeText(MainActivity.this,"led4 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3,0);}
                 break;
             // TODO: Veggie sandwich
         }
